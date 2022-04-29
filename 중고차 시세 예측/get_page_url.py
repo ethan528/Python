b196dccfdf33c16e25ec6c66d4a4725b99ee054b
ethan <ethan528@naver.com>
2022-04-29 16:42:56 +0900

    20220429

--- 중고차 시세 예측/get_page_url.py
+++ 중고차 시세 예측/get_page_url.py
@@ -5,29 +5,29 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 
-path = 'C:/Users/USER/TIL/chromedriver.exe'
-
-driver = webdriver.Chrome(executable_path=path, chrome_options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 def get_page_url(url):
-    data = []
-    
-    driver.get(url)
+    try:
+        data = []
+        
+        driver.get(url)
 
-    html = driver.page_source
+        html = driver.page_source
 
-    bs = BeautifulSoup(html,"html.parser")
+        bs = BeautifulSoup(html,"html.parser")
 
-    bs = bs.find('div', {'class':'cs-list02 cs-list02--ratio small-tp generalRegist'}).find('div', {'class':'list-in'})
+        bs = bs.find('div', {'class':'cs-list02 cs-list02--ratio small-tp generalRegist'}).find('div', {'class':'list-in'})
 
-    links = bs.find_all('a')
-    
-    for link in links:
-        if 'https:'+link.get('href') not in data:
-            data.append('https:' + link.get('href'))
-
+        links = bs.find_all('a')
+        
+        for link in links:
+            if 'https://www.kbchachacha.com'+link.get('href') not in data:
+                data.append('https://www.kbchachacha.com' + link.get('href'))
+    except:
+        data = []
     return(data)
 
-urls = get_page_url("https://www.kbchachacha.com/public/search/main.kbc#!?page=1&sort=-orderDate")
+urls = get_page_url("https://www.kbchachacha.com/public/search/main.kbc#!?makerCode=122&classCode=2446&carCode=2662&page=3&sort=-orderDate")
 print(urls)
 print(len(urls))
