76c03cc9aca139cd9e689f403ad09e50e4541b82
ethan <ethan528@naver.com>
2022-05-28 23:22:06 +0900

    20220528

--- Algorithm.py
+++ Algorithm.py
@@ -2,12 +2,3 @@
 RangeIndex: 275960 entries, 0 to 275959
 Data columns (total 3 columns):
  #   Column    Non-Null Count   Dtype 
----  ------    --------------   ----- 
- 0   index     275960 non-null  int64 
- 1   comments  275960 non-null  object # 전처리 후 데이터(특수문자제거, 이모지 제거 등), 빈 행 제거
- 2   likes     275960 non-null  int64  
-dtypes: int64(2), object(1)
-memory usage: 6.3+ MB
-
-# konlpy, hannanum, okt, komoran, kkma, re를 활용하여 텍스트 전처리
-#
