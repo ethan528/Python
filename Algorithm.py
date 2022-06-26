582f31cdbedc423cc17eecd3e742e655c8c73589
ethan <ethan528@naver.com>
2022-06-26 23:42:25 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,7 +1,15 @@
 a, b = map(int, input().split())
 c = int(input())
-t = 0
 
 
-if b+c >= 60 and a == 11:
-    a = t      
+if b+c >= 60 and a == 23:
+    a = 0
+    b = b+c-60
+if b+c >= 60:
+    a = a+1
+    b = b+c-60
+while b >= 60:
+    a = a+1
+    b = b-60
+
+print(a,b)
