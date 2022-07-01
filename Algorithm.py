ed113135d45a9476c1e8829c05ab0336f66a6f2e
ethan <ethan528@naver.com>
2022-07-01 23:14:58 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,1 +1,16 @@
+from string import ascii_uppercase
+
 W = input()
+
+n = [1,2,3,4,5,6,7,8,9,0]
+a = list(ascii_uppercase)
+
+p = {}
+cnt = 0
+
+for i in n:
+    for _ in range(3):
+        p.i = a[cnt]
+        cnt += 1
+
+print(p)
