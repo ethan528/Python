4440e0407735760dfd902377ac4ee12716cadffb
ethan <ethan528@naver.com>
2022-07-02 23:43:08 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,16 +1,21 @@
 from string import ascii_uppercase
 
-W = input()
+W = list(input())
 
-n = [1,2,3,4,5,6,7,8,9,0]
+n = [2,3,4,5,6,7,8,9]
 a = list(ascii_uppercase)
 
 p = {}
 cnt = 0
 
-for i in n:
-    for _ in range(3):
-        p.i = a[cnt]
-        cnt += 1
+for i in a:
+    if i == 7 or 9:
+        temp = 4
+    else:
+        temp = 3
+    for j in n:
+        for _ in range(temp):
+            print(i)
+            p[f'{i}'] = j
 
 print(p)
