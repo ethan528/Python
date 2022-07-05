9b003d923285efaaa59bd1c252f62a5e85f444b6
ethan <ethan528@naver.com>
2022-07-05 11:14:03 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -5,9 +5,11 @@ GW = 0
 for i in range(N):
     TS = 0
     for j in range(len(W[i])):
-        if W[i][j] not in W[i][j+2:] or W[i][j] not in W[i][j-2::-1]:
+        if W[i][j] not in W[i][j+2:] or W[i][j] not in W[i][j-2:]:
             TS += 1
-        if len(set(W[i][j])) == 1:
+        if len(set(W[i])) == len(W[i]):
+            TS += len(W[i])
+        if len(set(W[i])) == 1:
             TS += 1
             if TS == len(W[i]):
                 GW += 1
