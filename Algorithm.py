d6075a564df0af7d1f6cd7b362efa1e3efc34787
ethan <ethan528@naver.com>
2022-07-05 10:45:12 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,20 +1,15 @@
-from string import ascii_uppercase
+N = int(input())
+W = [input() for i in range(N)]
+GW = 0
 
-W = list(input())
+for i in range(N):
+    TS = 0
+    for j in range(len(W[i])):
+        if W[i][j] not in W[i][j+2:] or W[i][j] not in W[i][j-2::-1]:
+            TS += 1
+        if len(set(W[i][j])) == 1:
+            TS += 1
+            if TS == len(W[i]):
+                GW += 1
 
-n = [2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,9,9,9,9]
-a = list(ascii_uppercase)
-
-p = {}
-cnt = 0
-
-for i in range(len(a)):    
-    p[f"{a[i]}"] = n[i]
-
-for i in W:
-    if W != '':
-        cnt += 2
-    if p[f'{i}'] >= 2:
-        cnt += p[f'{i}']-1
-
-print(cnt)
+print(GW)
