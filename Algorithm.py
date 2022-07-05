4cebf47b4a5a170392f412d1afd40c3e25bd62f0
ethan <ethan528@naver.com>
2022-07-05 16:59:41 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -3,16 +3,10 @@ W = [input() for i in range(N)]
 GW = 0
 
 for i in range(N):
-    TS = 0
-    for j in range(len(W[i])):
-        try:
-            if W[i][j] != W[i][j+1] and W[i][j] == W[i][j+2]:
-                TS += 1
-        except IndexError:
-            TS += 1
-        if len(set(W[i])) == len(W[i]):
-            TS += 1
-            if len(W[i]) == TS:
-                GW += 1
+    if len(set(W[i])) == len(W[i]):
+        GW += 1
+    if len(W[i]) > 2:
+        if W[i] 
+
 
 print(GW)
