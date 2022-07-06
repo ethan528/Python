5591808d7e4bb7f56401053a58c6db42aad750d7
ethan <ethan528@naver.com>
2022-07-06 14:12:38 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,12 +1,13 @@
 N = int(input())
-W = [input() for i in range(N)]
 GW = 0
 
 for i in range(N):
-    if len(set(W[i])) == len(W[i]):
+    W = input()
+    if len(set(W)) == len(W):
         GW += 1
-    if len(W[i]) > 2:
-        if W[i] 
-
+    for j in W:
+        temp = W[j+1:]
+        F = temp.find(j)
+        if   
 
 print(GW)
