8eb2430288993f35c09a9cc6c92fc7d916ba00d1
ethan <ethan528@naver.com>
2022-07-06 16:48:51 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -3,11 +3,27 @@ GW = 0
 
 for i in range(N):
     W = input()
+    TS = 0
     if len(set(W)) == len(W):
         GW += 1
-    for j in W:
-        temp = W[j+1:]
-        F = temp.find(j)
-        if   
+        print('1',W)
+    for j in range(len(W)):
+        print(j)
+        K = W[j+1:]
+        print('k',K)
+        F = K.find(W[j])
+        print('f',F)
+        R = int(W.find(W[j])) + int(F)
+        print('r',R, j-1)
+        if R == (j-1) or R == j or len(K) == 1:
+            TS += 1
+            print(W[j], 'TS', TS, len(W))
+        elif K == '':
+            pass
+        else:
+            pass
+    if len(W) == TS:
+        GW += 1
+        print('2',W, 'GW', GW)
 
 print(GW)
