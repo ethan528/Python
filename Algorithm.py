279a438c6d4c0f20261776b34c07b0ec8b208146
ethan <ethan528@naver.com>
2022-07-07 11:06:23 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,29 +1,13 @@
 N = int(input())
-GW = 0
+M = 1
+F = 1
 
-for i in range(N):
-    W = input()
-    TS = 0
-    if len(set(W)) == len(W):
-        GW += 1
-        print('1',W)
-    for j in range(len(W)):
-        print(j)
-        K = W[j+1:]
-        print('k',K)
-        F = K.find(W[j])
-        print('f',F)
-        R = int(W.find(W[j])) + int(F)
-        print('r',R, j-1)
-        if R == (j-1) or R == j or len(K) == 1:
-            TS += 1
-            print(W[j], 'TS', TS, len(W))
-        elif K == '':
-            pass
-        else:
-            pass
-    if len(W) == TS:
-        GW += 1
-        print('2',W, 'GW', GW)
-
-print(GW)
+while N:
+    if N == 1:
+        print(F)
+    else:
+        M += 5 * M + F
+        F += 1
+        if  N <= M:
+            print(F)
+            break
