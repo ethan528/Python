74dc47b8a0f75d0d582c223f55c913157d3b4125
ethan <ethan528@naver.com>
2022-07-09 22:45:30 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,11 +1,2 @@
-board = [[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]
+X = int(input())
 
-answer = 0
-
-
-for i in range(len(board)):
-    for j in range(len(board)):
-        if board[i][j] != 0:
-            answer += 1
-
-print(answer)
