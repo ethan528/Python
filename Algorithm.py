cd709ed9eca7565e6faa1c8994976629cb546ad6
ethan <ethan528@naver.com>
2022-07-11 16:33:07 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,4 +1,15 @@
-a = ['1','2','3','4','5']
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+k = 2
 
-for n1, n2 in zip(a, a[1:]):
-    print(n1,n2)
+zl = [0 for _ in range(len(id_list))]
+bl = dict(zip(id_list, zl))
+wl = [i.split() for i in report]
+rl = []
+
+for i, n in enumerate(wl):
+    if n not in rl:
+        rl.append(n)
+        bl[n[1]] += 1
+el = [n[0] for i, n in enumerate(wl) if bl[n[1]] >= k]
+answer = [el.count(i) if el.count(i) else 0 for i in id_list]
