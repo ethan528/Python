93377252a0d14b435dcb511a55cab1bac7a67a7f
ethan <ethan528@naver.com>
2022-09-18 15:08:11 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -3,10 +3,11 @@ n = 10
 answer = []
 print(int(n**(1/2)))
 for i in range(1, int(n**(1/2)) + 1):
-    if (n % i == 0):
+    print('i', i)
+    if n % i == 0:
         answer.append(i) 
         print('i', i)
-        if ( (i**2) != n) : 
+        if i**2 != n : 
             answer.append(n // i)
             print('n//i', n//i)
             
