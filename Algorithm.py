847cf721502884eda1bf258423f44c456cf42b37
ethan <ethan528@naver.com>
2022-09-19 15:03:11 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,14 +1,4 @@
-n = 10
-
-answer = []
-print(int(n**(1/2)))
-for i in range(1, int(n**(1/2)) + 1):
-    print('i', i)
-    if n % i == 0:
-        answer.append(i) 
-        print('i', i)
-        if i**2 != n : 
-            answer.append(n // i)
-            print('n//i', n//i)
-            
-print(sum(answer))
+n = 156970273891890
+answer = list(map(int, str(n)))
+answer.reverse()
+print(answer)
