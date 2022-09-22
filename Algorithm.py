7ac52391258f74da8d3813e78c4617f1400d9657
ethan <ethan528@naver.com>
2022-09-23 00:36:42 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,11 +1,11 @@
-n = 3
-m= 12
-def solution(n, m):
-    gcd = lambda a,b : b if not a%b else gcd(b, a%b)
-    lcm = lambda a,b : a*b//gcd(a,b)
-    return [gcd(n, m), lcm(n, m)]
-
-print(solution(n, m))
-
-if not 3%3:
-    print(3%3)
+s = "try hello world"
+ans = ''
+for word in s.split(" "):
+    for i, p in enumerate(word):
+        print(i, p)
+        if i % 2 == 0:
+            ans += p.upper()
+        else:
+            ans += p.lower()
+    ans += ' '
+print(ans)
