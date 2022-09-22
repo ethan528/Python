7e5f18423c68a718d753bc53e3211ecfa9714b26
ethan <ethan528@naver.com>
2022-09-22 17:52:29 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,3 +1,11 @@
-arr = [4,3,2,1]
-arr.remove(1)
-print(arr)
+n = 3
+m= 12
+def solution(n, m):
+    gcd = lambda a,b : b if not a%b else gcd(b, a%b)
+    lcm = lambda a,b : a*b//gcd(a,b)
+    return [gcd(n, m), lcm(n, m)]
+
+print(solution(n, m))
+
+if not 3%3:
+    print(3%3)
