e1f7af9e30cc0d3709ca15de4370da7251f8eaa4
ethan <ethan528@naver.com>
2022-09-24 19:02:01 +0900

    update

--- Algorithm.py
+++ Algorithm.py
@@ -1,11 +1,1 @@
-s = "try hello world"
-ans = ''
-for word in s.split(" "):
-    for i, p in enumerate(word):
-        print(i, p)
-        if i % 2 == 0:
-            ans += p.upper()
-        else:
-            ans += p.lower()
-    ans += ' '
-print(ans)
+print(2//3)
