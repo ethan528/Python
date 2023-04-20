e528b63442ce2f8cd9168e71ae5257e9975a2b0f
ethan <ethan528@naver.com>
2023-04-20 11:23:07 +0900

    React update

---
+++ React/lecture1/src/App.3.js
@@ -0,0 +1,9 @@
+import MyComponent from "./MyComponent";
+
+const App3 = () =>{
+    return(
+        <MyComponent name = "React"></MyComponent>
+    )
+}
+
+export default App3;
---
+++ React/lecture1/src/MyComponent.js
@@ -0,0 +1,9 @@
+const MyComponent = props => {
+    return(
+    <>
+    <h1> {props.name} 함수형 다른 모습</h1>
+    </>
+    )
+}
+
+export default MyComponent;
